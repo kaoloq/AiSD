5a91765717d061024701916de68a680f85228d25
kaoloq <kushkoeva@yandex.ru>
2018-09-30 16:30:54 +0300

    added input from file and prichesala the code

--- 7381/KushkoevaAO/Lab1/Source/main.c
+++ 7381/KushkoevaAO/Lab1/Source/main.c
@@ -11,13 +11,13 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-void error(int i, int deep){
+void error(int i, int deep) {
         for(int i = 0; i <= deep; i++)
                 printf(" ");
 
 	printf("ERROR: ");
 
-	switch (i){								//вывод причины ошибки
+	switch (i) {								//вывод причины ошибки
 		case 1:
 			printf("invalid first character!\n");
 			break;
@@ -35,25 +35,22 @@ void error(int i, int deep){
 }
 
 
-int check_next(char* str, int deep){
-	if(str[deep] == '\0'){
+int check_next(char* str, int deep) {
+	if( str[deep] == '\0' ) {
 		error(2, deep);
 		return 0;
 	}
-
-	else
-		return 1;
+	else { return 1; }
 }
 
-int analiz(char* str, int deep){
+int analiz(char* str, int deep) {
 	int flag = 1;
 
-        if(str[deep] == '\0'){							//завершение рекурсии в случае константного выражения
-		return 1;
-	}
+        if(str[deep] == '\0') { return 1; }					//завершение рекурсии в случае константного выражения
 
 	for(int i = 0; i < deep; i++)						//отступ для удобного просмотра результата программы
 		printf(" ");
+
 	printf(">The incoming character: %c", str[deep]);
 
 	if(!isdigit(str[0])){ 							//проверка корректности первого элемента выражения
@@ -63,25 +60,25 @@ int analiz(char* str, int deep){
 		return 0;
 	}
 
-	if(isdigit(str[deep])){
+	if( isdigit(str[deep]) ) {
 		printf("\n");
-		if( !analiz(str, deep+1))
-			flag = 0;
+		if( !analiz(str, deep+1) ) { flag = 0; }
 	}
-	else{
-	        if(str[deep] == '+'|| str[deep] == '-' || str[deep] == '*'){
+	else {
+		if( str[deep] == '+'|| str[deep] == '-' || str[deep] == '*' ) {
 			printf("   \\\\ Waiting for a series of numbers.\n");
-			if(check_next(str, deep+1)){
-				if(!analiz(str, deep+1))
-					flag = 0;
+
+			if( check_next(str, deep+1) ) {
+				if( !analiz(str, deep+1) ) { flag = 0; }
 			}
-			else{
+
+			else {
 				flag = 0;
 				deep--;
 			}
 		}
 
-		else{
+		else {
 			printf("\n");
 			flag = 0;
 			error(3, deep);
@@ -90,23 +87,41 @@ int analiz(char* str, int deep){
 
 	for(int i = 0; i < deep; i++)						//отступ для удобного просмотра результата программы
 		printf(" ");
+
 	printf("<\n");
 
-	if(flag)
-		return 1;
-	else
-		return 0;
+	if( flag ) { return 1; }
+	else { return 0; }
 }
 
-int main(){
-	printf("Bracket Analyzer:\n");
+int main() {
+        int len_str = 100;
+        char* str = (char*)malloc(sizeof(char)*len_str);
 
-	int len_str = 100;
-	char* str = (char*)malloc(sizeof(char)*len_str);
 	scanf("%s", str);
 
-	if(analiz(str, 0))
-		printf("It's a constant expression!\n");
+	if ( strstr(str, ".txt") ) {
+		FILE* file = fopen(str, "r");
+
+		if (!file) {
+			printf("A file doesn't exist or it's not a constant expression!\n");
+			return 0;
+		}
+
+		str[0] = '\0';
+
+		fgets(str, len_str, file);
+
+		if ( str[strlen(str)-1] == '\n' )
+			str[strlen(str)-1] = '\0';
+
+		fclose(file);
+	}
+
+	printf("Constant expression analyzer:\n");
+
+	if( analiz(str, 0) )
+		 printf("It's a constant expression!\n");
 	else
 		printf("It's not a constant expression!\n");
 
